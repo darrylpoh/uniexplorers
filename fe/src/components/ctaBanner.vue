2481885596832fa3b52af6547e81758ee452d351
jjlim7 <jj.limmm159@gmail.com>
2023-10-04 15:40:07 +0800

    Add components for Uni Homepage

---
+++ fe/src/components/ctaBanner.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="bg-darkgreen">
+		<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+			<h2
+				class="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl text-white">
+				Exchange Program Community</h2>
+				<!-- [Join the forum discussion], [Connect,Learn,and Share]  -->
+
+			<p class="mb-8 text-base font-normal text-gray-100 lg:text-base md:px-6 lg:px-56">
+				Connect with Alumni and Current students to discover your ideal exchange program. Better yet, connect with fellow schoolmates embarking on the same exchange journey!</p>
+			<div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4 px-4">
+				<a href="#"
+					class="inline-flex justify-center items-center py-3 px-5 font-medium text-center text-darkgreen rounded-lg border-gray-300 focus:ring-4 focus:ring-gray-100 bg-lightgreen">
+					Join the Forum
+					<svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+						viewBox="0 0 14 10">
+						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M1 5h12m0 0L9 1m4 4L9 9" />
+					</svg>
+				</a>
+			</div>
+		</div>
+	</section>
+</template>
---
+++ fe/src/components/singleReview.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, reactive, computed } from 'vue';
+
+const props = defineProps(['review'])
+console.log(props.review)
+
+let showmore = false
+let comment = ref(props.review.comment_trunc)
+
+function onClickShow() {
+	showmore = !showmore
+	comment.value = showmore ? props.review.comment : props.review.comment_trunc
+}
+
+</script>
+
+<template>
+	<div class="flex flex-row flex-wrap md:h-1/2 sm:h-full">
+		<div class="xl:basis-1/6 lg:basis-1/5 md:basis-1/4 basis-full">
+			<img class="w-20 h-20 mb-3 rounded-full shadow-lg" src="@/assets/profile-placeholder.jpg" alt="Bonnie image" />
+		</div>
+		<div class="xl:basis-5/6 lg:basis-4/5 md:basis-3/4 basis-full">
+			<h3 class="font-semibold text-md">{{ review.name }}</h3>
+			<h3 class="text-md">{{ review.date }}</h3>
+		</div>
+		<div class="lg:basis-11/12 mb-3">
+			<p class="text-gray-500 text-sm md:text-md">{{ comment }}</p>
+			
+			<p class="mt-2 font-semibold underline underline-offset-4 text-gray-700" v-if="!showmore" @click="onClickShow">Show More</p>
+			<p class="mt-2 font-semibold underline underline-offset-4 text-gray-700" v-else @click="onClickShow">Show Less</p>
+		</div>
+	</div>
+</template>
