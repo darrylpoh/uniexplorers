005b9ed6d6ff2c0e8849a04e067210ccc5acfcd7
Jared Song <jskyejet@gmail.com>
2023-10-30 17:39:57 +0800

    hotfix

--- be/routes/forum_comments.js
+++ be/routes/forum_comments.js
@@ -87,7 +87,7 @@ module.exports = app => {
                 comment_obj.parent_id = parentId
             }
 
-            const thread_obj = await db('uni_forum_comment').insert(comment_obj).returning("*");
+            const thread_obj = await db('uni_forum_comment').insert(comment_obj).returning("*")[0];
 
             res.status(201).json({
                 ...thread_obj,
