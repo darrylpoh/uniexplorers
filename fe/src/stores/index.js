1de039582b44bfeb8add1a62a95ee9330122e625
A. Darryl <akeeladarryl@gmail.com>
2023-10-31 16:45:00 +0800

    smol change

--- fe/src/stores/index.js
+++ fe/src/stores/index.js
@@ -1,7 +1,6 @@
 export * from "./auth.store";
 export * from "./users.store";
 export * from "./searchStore";
-export * from "./threadStore";
 export * from "./uni.store"
 export * from "./cacheStore";
 export * from "./tagsStore"
